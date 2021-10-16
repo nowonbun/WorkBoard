34c10b617c2e8d3578c6368c4c8216c561752246
nowonbun <nowonbun@naver.com>
2021-10-17 01:13:24 +0900

    The library was updated.

--- main/java/common/LoggerManager.java
+++ main/java/common/LoggerManager.java
@@ -1,12 +1,13 @@
 package common;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+
 
 public class LoggerManager {
   private static LoggerManager instance = null;
@@ -22,16 +23,18 @@ public class LoggerManager {
   private LoggerManager() {
     flyweight = new HashMap<>();
     File file = new File(LocalPaths.getClassPath() + File.separator + "log4j.xml");
-    try (InputStream stream = new FileInputStream(file)) {
-      PropertyConfigurator.configure(stream);
-    } catch (Throwable e) {
-      throw new RuntimeException(e);
-    }
+    LoggerContext context = (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext(false);
+    context.setConfigLocation(file.toURI());
+    // try (InputStream stream = new FileInputStream(file)) {
+    // PropertyConfigurator.configure(stream);
+    // } catch (Throwable e) {
+    // throw new RuntimeException(e);
+    // }
   }
 
   public Logger get(Class<?> clazz) {
     if (!flyweight.containsKey(clazz)) {
-      flyweight.put(clazz, Logger.getLogger(clazz));
+      flyweight.put(clazz, LogManager.getLogger(clazz));
     }
     return flyweight.get(clazz);
   }
